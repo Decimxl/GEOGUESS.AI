062dcbee7a3eb0a3c57ec5ac7394c9d009a06467
MaxiB0nng <maxgravgaard@icloud.com>
2026-04-10 12:55:48 +0200

    numpy tic tak tow

--- board.py
+++ board.py
@@ -1,40 +1,37 @@
 import numpy as np
 
-# Opret et 3x3 bræt fyldt med nuller
+#makes a 3 *3 grid
 board = np.zeros((3, 3), dtype=int)
 print(board)
 
-# Funktion til at lave et træk på brættet
-def make_move(board, row, col, player):
-    if board[row, col] == 0:
-        board[row, col] = player
-        return True
-    return False
+#test board
+test = np.array([[1, 2, 3],
+                 [4, 5, 6],
+                 [7, 8, 9]])
 
-# Funktion til at tjekke om der er en vinder
-def check_winner(board):
-    for i in range(3):
-        if np.all(board[i, :] == 1) or np.all(board[:, i] == 1):
-            return 1
-        if np.all(board[i, :] == 2) or np.all(board[:, i] == 2):
-            return 2
-    if np.all(np.diag(board) == 1) or np.all(np.diag(np.fliplr(board)) == 1):
-        return 1
-    if np.all(np.diag(board) == 2) or np.all(np.diag(np.fliplr(board)) == 2):
-        return 2
-    return 0
+player = 1  
 
-# spil loop
-while True:
-    row = int(input(f"Player {player}, enter row (0-2): "))
-    col = int(input(f"Player {player}, enter column (0-2): "))
+def play():
+    global player
+
+    play = input(f"player {player} a move row:col")
+
+    row,col = map(int, play.split(":"))
     
-    if make_move(board, row, col, player):
-        print(board)
-        winner = check_winner(board)
-        if winner != 0:
-            print(f"Player {winner} wins!")
-            break
-        player = 2 if player == 1 else 1
+    if board[row,col] == 0:
+        board[row,col] = player
+
+        if player == 1:
+            player = 2
+        elif player == 2:
+            player = 1
     else:
-        print("Invalid move. Try again.")
+        print("You can't make that move")
+        
+    print(board)
+
+def colision(row,col):
+
+while np.all != 0:
+
+    play()
