30ee4b863e66729625e50c9d9ec00a4093abb534
MaxiB0nng <maxgravgaard@icloud.com>
2026-04-10 14:05:14 +0200

    tik tak toe logic final
    i hope XD

--- board.py
+++ board.py
@@ -4,34 +4,52 @@ import numpy as np
 board = np.zeros((3, 3), dtype=int)
 print(board)
 
-#test board
-test = np.array([[1, 2, 3],
-                 [4, 5, 6],
-                 [7, 8, 9]])
-
+winner = 0
 player = 1  
 
 def play():
-    global player
+    global player, win1 , win2,winner
 
     play = input(f"player {player} a move row:col")
 
-    row,col = map(int, play.split(":"))
-    
+    try:
+        row,col = map(int, play.split(":"))
+    except ValueError:
+        print("invalid input")
+        return
+
     if board[row,col] == 0:
         board[row,col] = player
-
-        if player == 1:
-            player = 2
-        elif player == 2:
-            player = 1
     else:
         print("You can't make that move")
         
+    i = 0
+    while i != 3:
+        if np.all(board[i,:] == player):
+            print (f"player {player} win")
+            winner = player
+        if np.all(board[:,i] == player):
+            print (f"player {player} win")
+            winner = player
+        if np.all(np.diag(board) == player):
+            print(f"player {player} win")
+            winner = player
+        if np.all(np.diag(np.fliplr(board)) == player):
+            print(f"player {player} win")
+            winner = player
+
+        i += 1 
+
+    if np.all(board != 0):
+        winner = "draw"
+
+    if player == 1:
+        player = 2
+    elif player == 2:
+        player = 1
+
+        
     print(board)
 
-def colision(row,col):
-
-while np.all != 0:
-
+while winner == 0:
     play()
